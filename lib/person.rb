d6457bdc4a05f113ae4159de28a21dd6dcf09829
mjester93 <m.jester.93@gmail.com>
2020-06-04 14:54:13 -0400

    Update

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,47 @@
 # your code goes here
+class Person
+    
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else
+            @happiness = happiness
+        end
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def happy?
+        return @happiness > 7
+    end
+
+    def clean?
+        return @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+end
