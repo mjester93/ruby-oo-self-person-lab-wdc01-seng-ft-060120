28b730fabe0a831a4319ce83e5569a291a7dcc72
mjester93 <m.jester.93@gmail.com>
2020-06-04 15:40:50 -0400

    adding .clamp to happiness and hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -12,23 +12,25 @@ class Person
     end
 
     def happiness=(happiness)
-        if happiness > 10
-            @happiness = 10
-        elsif happiness < 0
-            @happiness = 0
-        else
-            @happiness = happiness
-        end
+        @happiness = happiness.clamp(0, 10)
+        # if happiness > 10
+        #     @happiness = 10
+        # elsif happiness < 0
+        #     @happiness = 0
+        # else
+        #     @happiness = happiness
+        # end
     end
 
     def hygiene=(hygiene)
-        if hygiene > 10
-            @hygiene = 10
-        elsif hygiene < 0
-            @hygiene = 0
-        else
-            @hygiene = hygiene
-        end
+        @hygiene = hygiene.clamp(0, 10)
+        # if hygiene > 10
+        #     @hygiene = 10
+        # elsif hygiene < 0
+        #     @hygiene = 0
+        # else
+        #     @hygiene = hygiene
+        # end
     end
 
     def happy?
